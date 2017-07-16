f68291264f680d06de22719e08ff9a6ee38d4c53
m@rtlin2cz <martlin2cz@gmail.com>
2017-07-16 15:50:52 +0200

    Recreated application project
    	- created maven project fapapp
    	- created its submodule core
    		- with basic testing class "Symbol"

---
+++ application/core/src/main/java/cz/upol/fapapp/core/Symbol.java
@@ -0,0 +1,14 @@
+package cz.upol.fapapp.core;
+
+public class Symbol {
+	private final String value;
+
+	public Symbol(String value) {
+		super();
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+}
